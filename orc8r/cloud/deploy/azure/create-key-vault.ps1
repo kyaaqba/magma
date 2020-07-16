e423066fd7cbc68f8886898367a5e5729264ddad
Eric Grover <eric@ericgrover.com>
2020-07-16 11:49:47 -0400

    added secrets to script

--- orc8r/cloud/deploy/azure/create-key-vault.ps1
+++ orc8r/cloud/deploy/azure/create-key-vault.ps1
@@ -10,9 +10,13 @@ param(
 
         [Parameter()]
         [string]$location = 'eastus'
-
     )
 
 az keyvault create --location eastus --name $keyVaultName --resource-group $rgName
-az keyvault set-policy --name $keyVaultName --object-id $principalId --secret-permissions get list
-
+az keyvault set-policy --name $keyVaultName --object-id $principalId --secret-permissions get list set
+az keyvault secret set --vault-name $keyVaultName --name 'ServicePrincipalId' --value '[id value]'
+az keyvault secret set --vault-name $keyVaultName --name 'ServicePrincipalSecret' --value '[secret value]'
+az keyvault secret set --vault-name $keyVaultName --name 'ServerId' --value '[id value]'
+az keyvault secret set --vault-name $keyVaultName --name 'ServerSecret' --value '[secret value]'
+az keyvault secret set --vault-name $keyVaultName --name 'ClientId' --value '[id value]'
+az keyvault secret set --vault-name $keyVaultName --name 'ClientSecret' --value '[secret value]'
