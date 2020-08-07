6da70078b06597ef24b837ef6e23b8b41478a670
Pellegrene <Daniel.Pellegrene@bluechip-llc.com>
2020-08-07 10:06:30 -0400

    test

---
+++ orc8r/cloud/deploy/azure/get-cert.ps1
@@ -0,0 +1,1 @@
+az keyvault secret download -f downloaded-file2.txt --vault-name sonar-prod-magma-01 --id https://sonar-prod-magma-01.vault.azure.net/secrets/sonarlte-com/dabfd633beb74baea2dfefcd57a806cc --encoding base64
